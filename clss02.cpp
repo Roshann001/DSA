fc421dbcb8266e3a8c2403b803b88cc8df0d5c7d
Roshann001 <nigamroshan50@gmail.com>
2026-02-17 14:02:50 +0530

    clss21.cpp

---
+++ clss02.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int fibbo(int g)
+{
+ if(g==0 || g==1)
+ return g;
+else
+ return fibbo(g-1)+fibbo(g-2);
+}
+int main()
+{
+int n;
+ cin>>n;
+ cout<<fibbo(n)<<endl;
+ return 0;
+}
