2acf8730f2343f57931d55db40778da53da8480b
Roshann001 <nigamroshan50@gmail.com>
2026-02-18 16:00:04 +0530

    clss22.cpp

---
+++ clss22.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int a[100];
+
+    for(int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    // bubble sort logic
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n-i-1; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                int t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
+    }
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+
+    return 0;
+}
